050a0415601d4ff660c2dcd2eedc8ec1a6fa2010
Zpopeye <971234929@qq.com>
2019-08-05 17:51:05 +0800

    idea test

--- src/main/java/sz/zxl/com/demo/controller/EmpController.java
+++ src/main/java/sz/zxl/com/demo/controller/EmpController.java
@@ -1,23 +1,19 @@
 package sz.zxl.com.demo.controller;
 
-import java.util.List;
-
-import org.junit.runners.Parameterized.Parameters;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-
 import sz.zxl.com.demo.pojo.Dept;
 import sz.zxl.com.demo.pojo.Emp;
 import sz.zxl.com.demo.service.DeptService;
 import sz.zxl.com.demo.service.EmpService;
+
+import java.util.List;
 
 @Controller
 public class EmpController {
