1b7c5a34534f9672a8f12224df6044d139bca81e
carmeloo <971234929@qq.com>
2020-05-19 14:47:16 +0800

    rubbish

--- src/test/java/sz/zxl/com/demo/Test.java
+++ src/test/java/sz/zxl/com/demo/Test.java
@@ -10,6 +10,7 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,41 +18,8 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-        try {
-            List<String> warnings = new ArrayList<String>();
-            //是否覆盖现有文件
-            boolean overwrite = true;
-            //指定 逆向工程配置文件
-            //以流的形式读取配置文件
-            InputStream is = Test.class.getResourceAsStream("/MyBatisConfig.xml");
-            //创建配置文件解析器
-            ConfigurationParser cp = new ConfigurationParser(warnings);
-            //解析配置文件，得到所有配置信息
-            Configuration config = cp.parseConfiguration(is);
-            //生成了代码之后的回调
-            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-            //配置代码生成对象
-            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-            //执行代码生成操作
-            myBatisGenerator.generate(null);
-
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (XMLParserException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (InvalidConfigurationException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+       String sql = "ss";
+//       Connection connection = getConnection;
 
     }
 }
